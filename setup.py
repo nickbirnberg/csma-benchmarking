b6e678dde0c90b367945c1725cfba9856b633b8c
Nick Birnberg <nbirnberg@gmail.com>
2015-11-19 13:50:37 -0600

    Create project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ece438-mp4',
+    version='1.0.0',
+    packages=['mp4'],
+    url='https://github.com/nickbirnberg/ece438-mp4',
+    license='MIT',
+    author='nick birnberg and anit gandhi',
+    author_email='',
+    description='evaluates the performance of simplified CSMA protocols in a wired network'
+)
